08e46df1c90eb176427bed2dd2066877eb88c094
Greg Banda <grzegorz.banda@se.com>
2021-06-11 19:53:56 +0200

    [system] (feature): changed size of default item sheet

--- module/sheets/mutant-chronicles-item-sheet.js
+++ module/sheets/mutant-chronicles-item-sheet.js
@@ -1,4 +1,13 @@
 export default class MutantChroniclesItemSheet extends ItemSheet{
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 520,
+            height: 340,
+            classess: ["mutantchronicles", "sheet", "item"]
+        });
+    }
+
     get template(){
         return `systems/mutantchronicles/templates/sheets/${this.item.data.type}-sheet.html`;
     }
