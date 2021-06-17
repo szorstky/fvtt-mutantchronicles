8ece66d0e3231ec56c5f3654df91dbe9b7e4fa4c
Greg Banda <grzegorz.banda@se.com>
2021-06-17 22:56:03 +0200

    (fix): data object

--- module/actor/sheets/base.js
+++ module/actor/sheets/base.js
@@ -17,10 +17,25 @@ export default class MutantChroniclesActorSheet extends ActorSheet{
     /** @override */
     getData(options) {
 
+        // Basic data
+        let isOwner = this.actor.isOwner;
+        const data = {
+            owner: isOwner,
+            limited: this.actor.limited,
+            options: this.options,
+            editable: this.isEditable,
+            cssClass: isOwner ? "editable" : "locked",
+            isPlayer: this.actor.type === "character",
+            isNPC: this.actor.type === "npc",
+            isNemesis: this.actor.type === 'nemesis',
+            isEnemy: this.actor.type === 'enemy',
+            config: CONFIG.mutantchronicles,
+            rollData: this.actor.getRollData.bind(this.actor)
+        };
+
         const actorData = this.actor.data.toObject(false);
         data.actor = actorData;
         data.data = actorData.data;        
-        data.config = CONFIG.mutantchronicles;
         
         return data;
     }
