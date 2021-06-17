472d9c73c4ee26900b30e1ffe76f6f6b18c3e07e
Greg Banda <grzegorz.banda@se.com>
2021-06-17 22:33:19 +0200

    (fix): path to templates

--- module/templates.js
+++ module/templates.js
@@ -1,6 +1,6 @@
 export const preloadHandlebarsTemplates = async function() {
     const template_paths = [
-        "systems/mutantchronicles/templates/partials/mc-actor-stats-block.hbs"
+        "systems/mutantchronicles/templates/actors/partials/mc-actor-stats-block.hbs"
     ];
 
     return loadTemplates(template_paths);
