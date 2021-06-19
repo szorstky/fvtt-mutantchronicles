997e27d280e1869d64f0f114227bf6592556ab26
Greg Banda <grzegorz.banda@se.com>
2021-06-19 12:15:54 +0200

    (feature): skill item

--- module/config.js
+++ module/config.js
@@ -1,5 +1,16 @@
 export const mutantchronicles = {};
 
+mutantchronicles.attributeTypes = {
+    agility: "agility",
+    awerness: "awerness",
+    coordination: "coordination",
+    intelligence: "intelligence",
+    mental: "mental",
+    personality: "personality",
+    physique: "physique",
+    strength: "strength",
+};
+
 mutantchronicles.rangeTypes = {
     reach: "mutantchronicles.range.reach", 
     close: "mutantchronicles.range.close",
