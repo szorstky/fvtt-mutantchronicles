d2be8d4f97866c6868d62a0808c3dedb75b02505
Greg Banda <grzegorz.banda@se.com>
2021-07-14 22:04:52 +0200

    [player] (chore) layout

--- module/templates.js
+++ module/templates.js
@@ -1,6 +1,7 @@
 export const preloadHandlebarsTemplates = async function() {
     const template_paths = [
         "systems/mutantchronicles/templates/actors/partials/player-attributes-and-skills-partial.hbs",
+        "systems/mutantchronicles/templates/actors/partials/player-attributes.hbs",
         "systems/mutantchronicles/templates/actors/partials/mc-actor-stats-block.hbs"
     ];
 
